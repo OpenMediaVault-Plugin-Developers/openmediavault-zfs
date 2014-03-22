670691f3b555344a849ad9911814a78949c1b31b
Niclas Berglind <nb@kjam.se>
2014-03-22 14:14:33 +0100

    Fixed some commands to redirect stderr to stdout.
    Signed-off-by: Niclas Berglind <nb@kjam.se>

--- src/Snapshot.php
+++ src/Snapshot.php
@@ -41,7 +41,7 @@ class OMVModuleZFSSnapshot {
 	public function __construct($name) {
 		$this->name = $name;
 		$qname = preg_quote($name, '/');
-		$cmd = "zfs list -H -t snapshot";
+		$cmd = "zfs list -H -t snapshot 2>&1";
 		$this->exec($cmd, $out, $res);
 		foreach ($out as $line) {
 			if (preg_match('/^' . $qname . '\t.*$/', $line)) {
@@ -93,7 +93,7 @@ class OMVModuleZFSSnapshot {
 	 */
 	public function setProperties($properties) {
 		foreach ($properties as $newpropertyk => $newpropertyv) {
-			$cmd = "zfs set " . $newpropertyk . "=" . $newpropertyv . " " . $this->name;
+			$cmd = "zfs set " . $newpropertyk . "=" . $newpropertyv . " " . $this->name . " 2>&1";
 			$this->exec($cmd,$out,$res);
 			$this->updateProperty($newpropertyk);
 		}
@@ -107,7 +107,7 @@ class OMVModuleZFSSnapshot {
 	 * @access private
 	 */
 	private function updateProperty($property) {
-		$cmd = "zfs get -H " . $property . " " . $this->name;
+		$cmd = "zfs get -H " . $property . " " . $this->name . " 2>&1";
 		$this->exec($cmd,$out,$res);
 		$tmpary = preg_split('/\t+/', $out[0]);
 		$this->properties["$tmpary[1]"] = array("value" => $tmpary[2], "source" => $tmpary[3]);
@@ -120,7 +120,7 @@ class OMVModuleZFSSnapshot {
 	 * @access private
 	 */
 	private function updateAllProperties() {
-		$cmd = "zfs get -H all " . $this->name;
+		$cmd = "zfs get -H all " . $this->name . " 2>&1";
 		$this->exec($cmd,$out,$res);
 		unset($this->properties);
 		foreach ($out as $line) {

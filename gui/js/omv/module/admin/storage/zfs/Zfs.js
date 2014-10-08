19986fd29e771b61b0c1afcc4a6816c7e6e7f7ff
Niclas Berglind <nb@kjam.se>
2014-10-08 22:05:24 +0200

    Changed some icons.
    Signed-off-by: Niclas Berglind <nb@kjam.se>

--- gui/js/omv/module/admin/storage/zfs/Zfs.js
+++ gui/js/omv/module/admin/storage/zfs/Zfs.js
@@ -2,7 +2,7 @@ OMV.WorkspaceManager.registerNode({
     id      : "zfs",
     path    : "/storage",
     text    : _("ZFS"),
-    icon16  : "images/zfs.png",
-    iconSvg : "images/zfs.svg"
+    icon16  : "images/zfs_main.png",
+    iconSvg : "images/zfs_main.svg"
 });
 

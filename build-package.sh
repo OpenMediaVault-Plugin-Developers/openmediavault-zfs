b4495c30c275d9c4988cc9718a8ad8ff75633cd6
Aaron Murray <plugins@omv-extras.org>
2014-10-16 06:22:53 -0500

    add files for locale support

--- build-package.sh
+++ build-package.sh
@@ -5,10 +5,12 @@ rm -rf openmediavault-zfs/var/www/openmediavault/js/omv/module/admin/storage/zfs
 mkdir -p openmediavault-zfs/var/www/openmediavault/js/omv/module/admin/storage/zfs
 rm -rf openmediavault-zfs/var/www/openmediavault/images
 mkdir -p openmediavault-zfs/var/www/openmediavault/images
+rm -rf openmediavault-zfs/usr/share/openmediavault/locale
 cp src/* openmediavault-zfs/usr/share/openmediavault/engined/rpc/zfs
 cp gui/rpc/zfs.inc openmediavault-zfs/usr/share/openmediavault/engined/rpc
 cp gui/js/omv/module/admin/storage/zfs/* openmediavault-zfs/var/www/openmediavault/js/omv/module/admin/storage/zfs
 cp images/* openmediavault-zfs/var/www/openmediavault/images
+cp -r usr/share/openmediavault/locale openmediavault-zfs/usr/share/openmediavault/
 cd openmediavault-zfs
 dpkg-buildpackage -us -uc
 

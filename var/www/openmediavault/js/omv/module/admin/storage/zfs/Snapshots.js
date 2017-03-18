97c78494273a84acb54f574b4d092e32aeb81d0f
Aaron Murray <plugins@omv-extras.org>
2017-03-18 12:11:17 -0500

    improve foreign content handling

--- var/www/openmediavault/js/omv/module/admin/storage/zfs/Snapshots.js
+++ var/www/openmediavault/js/omv/module/admin/storage/zfs/Snapshots.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2014-2017 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 // require("js/omv/WorkspaceManager.js")
 // require("js/omv/workspace/grid/Panel.js")
 // require("js/omv/Rpc.js")
@@ -26,21 +42,25 @@ Ext.define("OMV.module.admin.storage.zfs.Snapshots", {
 	stateId: "d14ac294-f5bd-11e6-a856-4a49d0dfdf2b",
 	selModel: "checkboxmodel",
 	columns: [{
+        xtype: "textcolumn",
 		text: _("Filesystem/Voulume"),
 		dataIndex: 'parent',
 		sortable: true,
 		stateId: 'parent',
 	},{
+        xtype: "textcolumn",
 		text: _("Name"),
 		dataIndex: 'name',
 		sortable: true,
 		stateId: 'name',
 	},{
+        xtype: "textcolumn",
 		text: _("Used"),
 		dataIndex: 'used',
 		sortable: true,
 		stateId: 'used'
 	},{
+        xtype: "textcolumn",
 		text: _("Refer"),
 		dataIndex: 'refer',
 		sortable: true,
@@ -180,7 +200,7 @@ Ext.define("OMV.module.admin.storage.zfs.Snapshots", {
 			}
 		});
 	},
-	
+
 	onRollbackButton: function() {
 		var me = this;
 		var sm = me.getSelectionModel();
--- var/www/openmediavault/js/omv/module/admin/storage/zfs/TreePanel.js
+++ var/www/openmediavault/js/omv/module/admin/storage/zfs/TreePanel.js
@@ -1,22 +1,18 @@
 /**
- * This file is part of OpenMediaVault.
+ * Copyright (C) 2014-2017 OpenMediaVault Plugin Developers
  *
- * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
- * @author    Volker Theile <volker.theile@openmediavault.org>
- * @copyright Copyright (c) 2009-2014 Volker Theile
- *
- * OpenMediaVault is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * (at your option) any later version.
  *
- * OpenMediaVault is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 // require("js/omv/tree/Panel.js")
 // require("js/omv/grid/Panel.js")
@@ -450,7 +446,7 @@ Ext.define("OMV.module.admin.storage.zfs.TreePanel", {
 			tbarBtnHidden["down"] = true;
 			tbarBtnHidden["rollback"] = true;
 		}
-		
+
 		// Update the button controls.
 		Ext.Array.each(tbarBtnName, function(name) {
 			var tbarBtnCtrl = me.queryById(me.getId() + "-" + name);

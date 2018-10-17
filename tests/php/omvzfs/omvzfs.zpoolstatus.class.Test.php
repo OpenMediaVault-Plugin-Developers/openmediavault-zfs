b6c60911cd69c8071ebe22b25e4fab1018a9e196
Michal Dziekonski <glibnes@gmail.com>
2018-10-18 01:04:23 +0200

    Replace ZpoolStatus class mocks with real data

--- tests/php/omvzfs/omvzfs.zpoolstatus.class.Test.php
+++ tests/php/omvzfs/omvzfs.zpoolstatus.class.Test.php
@@ -5,10 +5,9 @@ require_once 'PHPUnit/Autoload.php';
 // Relative "base" path used
 $filePath = dirname(__FILE__);
 
+require_once $filePath . "/utils.php";
 require_once $filePath . "/../../../usr/share/omvzfs/ZpoolStatus.php";
 require_once $filePath . "/../../../usr/share/omvzfs/VdevState.php";
-
-require_once $filePath . "/mocks/test_omvzfs_zpoolstatus/cmdoutput_mocks.php";
 
 class OMVZFSZpoolStatusClassTest extends \PHPUnit\Framework\TestCase {
     /**
@@ -36,78 +35,158 @@ class OMVZFSZpoolStatusClassTest extends \PHPUnit\Framework\TestCase {
     }
 
     public function allDevicesGetterDataProvider() {
-        return [
-            "simple, healthy pool's output" => [
-                Test\OMVZFS\ZpoolStatus\Mocks\SimpleMock::getCmdOutput(),
-                [
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1"
-                ]
-            ],
-            "complex pool's output (vdevs, logs & spares)" => [
-                Test\OMVZFS\ZpoolStatus\Mocks\AdvancedMock::getCmdOutput(),
-                [
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-a6c3b929-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "12655960456386485198",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1"
-                ]
-            ]
+        $mocks = [
+            // Simple cases where devices are either referenced as:
+            // * /dev/sdXY
+            // * /dev/nvme0nXpY
+            // * /dev/disk/by-id
+            "cmd_output.zpool_status.simplepool_sata_bydev",
+            "cmd_output.zpool_status.simplepool_sata_byid",
+            "cmd_output.zpool_status.simplepool_nvme_bydev",
+            "cmd_output.zpool_status.simplepool_nvme_byid",
+            "cmd_output.zpool_status.simplepool_mixed_bydev",
+            "cmd_output.zpool_status.simplepool_mixed_byid",
+            // A bit longer list with more than one nested groups
+            "cmd_output.zpool_status.redundantpool_raidz3_double",
+            "cmd_output.zpool_status.redundantpool_mixed",
+            // Complex pool structure, with caches, logs & spares
+            "cmd_output.zpool_status.complexpool_createdupfront",
+            // Degraded pools with missing devices
+            // (where device GUID is displayed instead of its path)
+            "cmd_output.zpool_status.degradedpool_simplemirror",
+            "cmd_output.zpool_status.degradedpool_multidegradation",
         ];
+
+        $datasets = [];
+
+        foreach ($mocks as $mockName) {
+            $datasets[$mockName] = [
+                // mock file content
+                OMVZFSTestUtils::loadCmdOutputFile(
+                    "/mocks/omvzfs.zpoolstatus/" . $mockName . ".txt"
+                ),
+                // mock expected structure
+                OMVZFSTestUtils::loadJSONFile(
+                    "/expectations/omvzfs.zpoolstatus/all_devices/" . $mockName . ".json"
+                )
+            ];
+        }
+
+        return $datasets;
     }
 
     public function allDevicesWithExcludesGetterDataProvider() {
-        return [
-            "simple, healthy pool's output" => [
-                Test\OMVZFS\ZpoolStatus\Mocks\SimpleMock::getCmdOutput(),
-                [
-                    OMVModuleZFSVdevState::STATE_ONLINE
-                ],
-                []
-            ],
-            "complex pool's output (vdevs, logs & spares) (exclude SPARE_INUSE)" => [
-                Test\OMVZFS\ZpoolStatus\Mocks\AdvancedMock::getCmdOutput(),
-                [
-                    OMVModuleZFSVdevState::STATE_SPARE_INUSE
-                ],
-                [
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-a6c3b929-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "12655960456386485198",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1"
-                ]
-            ],
-            "complex pool's output (vdevs, logs & spares) (exclude SPARE_INUSE & UNAVAIL)" => [
-                Test\OMVZFS\ZpoolStatus\Mocks\AdvancedMock::getCmdOutput(),
-                [
-                    OMVModuleZFSVdevState::STATE_SPARE_INUSE,
+        $testcases = [
+            // Simple case, with no devices excluded (because all are ONLINE)
+            "cmd_output.zpool_status.simplepool_sata_bydev (excluded: UNAVAIL)" => [
+                "mockfile" => "cmd_output.zpool_status.simplepool_sata_bydev",
+                "expectationfile" => "cmd_output.zpool_status.simplepool_sata_bydev.exclude_unavail",
+                "excludedStates" => [
                     OMVModuleZFSVdevState::STATE_UNAVAIL
-                ],
-                [
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-a6c3b929-part2",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VB84ca63f0-93542ba0-part1",
-                    "/dev/disk/by-id/ata-VBOX_HARDDISK_VBefaa040c-71d3f044-part1"
                 ]
-            ]
+            ],
+            // Simple case, with all devices excluded (because all are ONLINE)
+            "cmd_output.zpool_status.simplepool_sata_bydev (excluded: ONLINE)" => [
+                "mockfile" => "cmd_output.zpool_status.simplepool_sata_bydev",
+                "expectationfile" => "cmd_output.zpool_status.simplepool_sata_bydev.exclude_online",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_ONLINE
+                ]
+            ],
+            // Complex case, with no devices excluded (because none are OFFLINE)
+            "cmd_output.zpool_status.complexpool_createdupfront (excluded: OFFLINE)" => [
+                "mockfile" => "cmd_output.zpool_status.complexpool_createdupfront",
+                "expectationfile" => "cmd_output.zpool_status.complexpool_createdupfront.exclude_offline",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_OFFLINE
+                ]
+            ],
+            // Complex case, with all spares excluded (because all spares are AVAIL)
+            "cmd_output.zpool_status.complexpool_createdupfront (excluded: SPARE_AVAIL)" => [
+                "mockfile" => "cmd_output.zpool_status.complexpool_createdupfront",
+                "expectationfile" => "cmd_output.zpool_status.complexpool_createdupfront.exclude_spare_avail",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_SPARE_AVAIL
+                ]
+            ],
+            // Complex case, with all devices excluded (both SPARE_AVAIL & ONLINE are excluded)
+            "cmd_output.zpool_status.complexpool_createdupfront (excluded: SPARE_AVAIL & ONLINE)" => [
+                "mockfile" => "cmd_output.zpool_status.complexpool_createdupfront",
+                "expectationfile" => "cmd_output.zpool_status.complexpool_createdupfront.exclude_spare_avail_online",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_SPARE_AVAIL,
+                    OMVModuleZFSVdevState::STATE_ONLINE
+                ]
+            ],
+            // Degraded simple pool case, with no devices excluded (because none are OFFLINE)
+            "cmd_output.zpool_status.degradedpool_simplemirror (excluded: OFFLINE)" => [
+                "mockfile" => "cmd_output.zpool_status.degradedpool_simplemirror",
+                "expectationfile" => "cmd_output.zpool_status.degradedpool_simplemirror.exclude_offline",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_OFFLINE
+                ]
+            ],
+            // Degraded simple pool case, with some devices excluded (UNAVAIL ones)
+            "cmd_output.zpool_status.degradedpool_simplemirror (excluded: UNAVAIL)" => [
+                "mockfile" => "cmd_output.zpool_status.degradedpool_simplemirror",
+                "expectationfile" => "cmd_output.zpool_status.degradedpool_simplemirror.exclude_unavail",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_UNAVAIL
+                ]
+            ],
+            // Degraded simple pool case, with all devices excluded (both UNAVAIL & ONLINE are excluded)
+            "cmd_output.zpool_status.degradedpool_simplemirror (excluded: UNAVAIL & ONLINE)" => [
+                "mockfile" => "cmd_output.zpool_status.degradedpool_simplemirror",
+                "expectationfile" => "cmd_output.zpool_status.degradedpool_simplemirror.exclude_unavail_online",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_UNAVAIL,
+                    OMVModuleZFSVdevState::STATE_ONLINE
+                ]
+            ],
+            // Degraded multidegradation pool case, with no devices excluded (because none are OFFLINE)
+            "cmd_output.zpool_status.degradedpool_multidegradation (excluded: OFFLINE)" => [
+                "mockfile" => "cmd_output.zpool_status.degradedpool_multidegradation",
+                "expectationfile" => "cmd_output.zpool_status.degradedpool_multidegradation.exclude_offline",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_OFFLINE
+                ]
+            ],
+            // Degraded multidegradation pool case, with some devices excluded (UNAVAIL ones)
+            "cmd_output.zpool_status.degradedpool_multidegradation (excluded: UNAVAIL)" => [
+                "mockfile" => "cmd_output.zpool_status.degradedpool_multidegradation",
+                "expectationfile" => "cmd_output.zpool_status.degradedpool_multidegradation.exclude_unavail",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_UNAVAIL
+                ]
+            ],
+            // Degraded multidegradation pool case, with some devices excluded (UNAVAIL & SPARE_AVAIL ones)
+            "cmd_output.zpool_status.degradedpool_multidegradation (excluded: UNAVAIL & SPARE_AVAIL)" => [
+                "mockfile" => "cmd_output.zpool_status.degradedpool_multidegradation",
+                "expectationfile" => "cmd_output.zpool_status.degradedpool_multidegradation.exclude_unavail_spare_avail",
+                "excludedStates" => [
+                    OMVModuleZFSVdevState::STATE_UNAVAIL,
+                    OMVModuleZFSVdevState::STATE_SPARE_AVAIL
+                ]
+            ],
         ];
+
+        $datasets = [];
+
+        foreach ($testcases as $testcaseName => $testcaseConfig) {
+            $datasets[$testcaseName] = [
+                // testcase mock file content
+                OMVZFSTestUtils::loadCmdOutputFile(
+                    "/mocks/omvzfs.zpoolstatus/" . $testcaseConfig["mockfile"] . ".txt"
+                ),
+                // testcase exclusion table
+                $testcaseConfig["excludedStates"],
+                // testcase mock expected structure
+                OMVZFSTestUtils::loadJSONFile(
+                    "/expectations/omvzfs.zpoolstatus/all_devices_with_exclusion/" . $testcaseConfig["expectationfile"] . ".json"
+                )
+            ];
+        }
+
+        return $datasets;
     }
 }

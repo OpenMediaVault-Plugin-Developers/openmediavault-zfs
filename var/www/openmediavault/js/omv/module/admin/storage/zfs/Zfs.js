0a09a0d43b3bbe072edd716e2045930b0793606f
Aaron Murray <plugins@omv-extras.org>
2021-10-30 11:02:15 -0500

    update for OMV 6.x - web interface is not ported yet!

--- var/www/openmediavault/js/omv/module/admin/storage/zfs/Zfs.js
+++
@@ -1,9 +0,0 @@
-// require("js/omv/WorkspaceManager.js")
-OMV.WorkspaceManager.registerNode({
-    id      : "zfs",
-    path    : "/storage",
-    text    : _("ZFS"),
-    icon16  : "images/zfs_main.png",
-    iconSvg : "images/zfs_main.svg"
-});
-
